2ec357f0b2afa9c0baee6901205e183a463b3ae2
satya sindhu <movva001@gmail.com>
2022-01-23 13:47:26 -0500

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import "./styles.scss";
 import Header from "components/Appointment/Header.js";
@@ -6,6 +7,9 @@ import Empty from "components/Appointment/Empty.js";
 
 export default function Appointment(props) {
     return (
-        <article className="appointment">{props.time  ? `Appointment at ${props.time}`: 'No Appointments'}</article>
+        <article className="appointment">
+            <Header time={props.time} />
+            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+        </article>
     )
 }
