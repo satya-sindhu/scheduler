68ce081ce3ab9923d44204ae497628c4a69c55a1
satya sindhu <movva001@gmail.com>
2022-01-24 17:16:57 -0500

    Retrieving Appointments usingPromise

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/jsx-no-undef */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-// import getAppointmentsForDay from "helpers/selectors";
+import getAppointmentsForDay from "helpers/selectors";
 
 const days = [
   {
@@ -66,13 +64,12 @@ const appointments = [
 
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   })
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
 
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
@@ -84,15 +81,23 @@ export default function Application(props) {
 
   useEffect(() => {
     axios.get(`/api/days`).then((response) => {
-      console.log(response);
       setDays(response.data);
-      // setState((prev) => ({
-      //   ...prev,
-      //   days: response[0].data,
-      //   appointments: response[1].data,
-      // }));
-    });
-  }, []);
+      const baseURL = "http://localhost:8001"
+      const daysURL = axios.get(`${baseURL}/api/days`)
+      const appointmentURL = axios.get(`${baseURL}/api/appointments`)
+      const interviewersURL = axios.get(`${baseURL}/api/interviewers`)
+      Promise.all([daysURL, appointmentURL, interviewersURL])
+        .then(response => {
+          setState(prev => ({
+            ...prev,
+            days: response[0].data,
+            appointments: response[1].data,
+            interviewers: response[2].data
+          }));
+        });
+    }, []);
+  })
+
 
   return (
     <main className="layout">
@@ -115,11 +120,18 @@ export default function Application(props) {
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-        <Appointment key="last" time="5pm" />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />)}
+        {dailyAppointments.map((appointment) => (
+          <Appointment
+            key={appointment.id}
+            id={appointment.id}
+            time={appointment.time}
+            interview={appointment.interview}
+          />
+        ))}
+        < Appointment key='last' time='5pm' />
       </section>
     </main>
   )
-};
+}
