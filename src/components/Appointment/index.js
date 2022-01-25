8a1b0db5dd60d29e7f30b254b04cf4c937fa5b86
satya sindhu <movva001@gmail.com>
2022-01-25 16:57:15 -0500

    Using our Custom Hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,38 @@ import "./styles.scss";
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form.js";
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-    return (
-        <article className="appointment">
-            <Header time={props.time} />
-            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
-        </article>
+    console.log(props);
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
     )
+    console.log('mode', mode);
+    return (
+        <article className='appointment'>
+            <Header time={props.time} />
+            {/* {props.interview ? <Show /> : <Empty />} */}
+            {mode === EMPTY &&
+                <Empty onAdd={() => transition(CREATE)} />}
+            {mode === CREATE && (
+                <Form
+                    interviewers={[]}
+                    onCancel={() => back()} />
+            )}
+            {mode === SHOW && (
+                <Show
+                    student={props.interview.student}
+                    interviewer={props.interview.interviewer}
+                />
+            )}
+        </article>
+
+    );
 }
