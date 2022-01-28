7006d73ef9a9947b609ae04e08b41c9ad47f0670
satya sindhu <movva001@gmail.com>
2022-01-27 22:29:25 -0500

    Navigating Days Cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,14 @@ describe("Navigation", () => {
     it("should visit root", () => {
         cy.visit("/");
     });
+
+    it("should navigate to Tuesday", () => {
+        cy.visit("/");
+
+        cy.contains("li", "Tuesday")
+        cy.contains("[data-testid=day]", "Tuesday")
+            .click()
+            .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected");
+    });
 });
